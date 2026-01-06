90cc9acd984afa8b3944167152e9049faabe041e
NeoZhang0506 <zhang358247@whut.edu.cn>
2026-01-06 21:37:44 +0800

    C practice

---
+++ 2026_01_06/practice.c
@@ -0,0 +1,24 @@
+//C Primer Plus P208
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+////1
+//int main()
+//{
+//	int ch, cnt;
+//	cnt = 0;
+//
+//	while ((ch = getchar() != EOF))
+//	{
+//		++cnt;
+//	}
+//	printf("%d", cnt);
+//	return 0;
+//}
+
+//2
+int main()
+{	
+	
+	return 0;
+}
